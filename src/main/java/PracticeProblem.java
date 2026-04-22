d4b5a57a613521353680ed6a20b4810cb5139b89
AlexanderAuton <350319867@tdsb.ca>
2026-04-22 15:16:38 +0000

    StartingTheLoopUnitNow

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,14 @@
+/**
+
+        * File: Lesson 3.5 - montyHall.Mp4
+
+        * Author: Alex
+
+        * Date Created: March 30, 2026
+
+        * Date Last Modified: April 21, 2026
+
+        */
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -23,5 +34,40 @@ public class PracticeProblem {
 	public static void q5() {
 		//Write question 5 code here
 	}
+public static String getFirstName(String fn){
+	fn = fn.trim();
+	int index = fn.indexOf(" ");
+	String fname = fn.substring(0, index);
+	return fname;
+	}
 
+public static String getLastName(String ln){
+	ln = ln.trim();
+	int index = ln.lastIndexOf(" ");
+	String lname = ln.substring(index + 1);
+	return lname;
+	}
+
+public static boolean isValidName(String bool){
+	String fn;
+	String ln;
+	bool = bool.trim();
+int index = bool.indexOf(" ");
+int index2 = bool.lastIndexOf(" ");
+if (index == -1) {
+	return false;
+}
+if (index == index2){
+	fn = bool.substring(0, index);
+	ln = bool.substring(1+index);
+	if (fn.length()>=2 && ln.length()>=2){
+	return true;
+	} else {
+		return false;
+	}
+}
+else {
+	return false;
+ }
+}
 }
